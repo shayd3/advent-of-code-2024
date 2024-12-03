df7953f9a51052b34633634351a546c0312b607d
shayd3 <ryan.ross215@gmail.com>
2024-12-02 22:22:19 -0500

    day 2 init, update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # advent-of-code-2024
 AoC 2024
+
+## Running a day
+```bash
+python solve.py {day number here}
+```
+
+i.e.
+```bash
+python solve.py 2
+```
+will run the solution under `day_2` folder
+
+## using aoc-cli to get inputs
+```bash
+aoc download --input-file day_{number here}/input.txt
+```
---
+++ day_2/day_2.py
@@ -0,0 +1,16 @@
+from day import Day
+
+class Day_2(Day):
+    def __init__(self):
+        self.input = self.puzzle_input()
+
+    def parse_input(self):
+        return self.input
+
+    def part1(self):
+        """Solve part 1."""
+        return 0
+
+    def part2(self):
+        """Solve part 2."""
+        return 0
