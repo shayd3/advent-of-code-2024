d8cd19eff387039397745e4dd5aed9e5c69f82e3
shayd3 <ryan.ross215@gmail.com>
2024-12-03 20:11:51 -0500

    Day 2 Part 1 - Done!

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,15 +1,28 @@
 from day import Day
-
+from utilities.parse import Parse
 class Day_2(Day):
     def __init__(self):
         self.input = self.puzzle_input()
 
     def parse_input(self):
-        return self.input
+        return [list(map(int, report.split())) for report in self.input.splitlines()]
 
     def part1(self):
         """Solve part 1."""
-        return 0
+        reports = self.parse_input()
+        safe_reports = 0
+
+        for report in reports:
+            increasing_or_decreasing = (report == sorted(report) or report == sorted(report, reverse=True))
+            is_within_range = True
+            for i in range(len(report)-1):
+                level_diff = abs(report[i] - report[i+1])
+                if not 1 <= level_diff <= 3:
+                    is_within_range = False
+            if increasing_or_decreasing and is_within_range:
+                safe_reports += 1
+
+        return safe_reports
 
     def part2(self):
         """Solve part 2."""
