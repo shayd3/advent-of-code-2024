69af2af9a3137129616c58421f7bbd513c6fc246
shayd3 <ryan.ross215@gmail.com>
2024-12-03 21:46:01 -0500

    Modify day class to include example.txt option for testing

--- day.py
+++ day.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 class Day(ABC):
-    def puzzle_input(self):
+    def puzzle_input(self, is_example_input = False):
         """Grabs the input file for the day which is at the root of the folder"""
         # Get the directory of the current module
         module_dir = Path(self.__class__.__module__.replace('.', '/')).parent
         # Construct the path to the input.txt file within the same directory
-        input_file = module_dir / "input.txt"
+        input_file = module_dir / "example.txt" if (is_example_input) else module_dir / "input.txt"
         return input_file.read_text().strip()
 
     @abstractmethod
