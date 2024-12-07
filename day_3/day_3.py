5e16e4a1d8caa699fde7ecd09bc512acf350e0d0
shayd3 <ryan.ross215@gmail.com>
2024-12-07 08:18:25 -0500

    Day 3 Part 2 - Done!

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -4,13 +4,33 @@ class Day_3(Day):
     def __init__(self):
         self.input = self.puzzle_input(is_example_input=False)
 
+    def find_mul(self, string):
+        pattern = r"mul\(\d+,\d+\)"
+        return re.findall(pattern, string)
+
+    def find_mul_with_toggle(self, string):
+        pattern = r"mul\(\d+,\d+\)|do(?:n't)?\(\)"
+        return re.findall(pattern, string)
+
     def part1(self):
         result = 0
-        found = re.findall("mul\(\d+,\d+\)",self.input)
+        found = self.find_mul(self.input)
         for f in found:
             nums = re.findall("\d+",f)
             result += int(nums[0]) * int(nums[1])
         return result
 
     def part2(self):
-        return 0
+        result = 0
+        found = self.find_mul_with_toggle(self.input)
+        mul_enabled = True
+        for f in found:
+            if "mul" in f:
+                if mul_enabled:
+                    nums = re.findall("\d+",f)
+                    result += int(nums[0]) * int(nums[1])
+            elif "don't" in f:
+                mul_enabled = False
+            elif "do" in f:
+                mul_enabled = True
+        return result
