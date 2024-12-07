bbd6761a43f46a0e5df569884c466722dd873ad1
shayd3 <ryan.ross215@gmail.com>
2024-12-07 07:51:50 -0500

    Day 3 Part 1 done!

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,10 +1,16 @@
 from day import Day
+import re
 class Day_3(Day):
     def __init__(self):
         self.input = self.puzzle_input(is_example_input=False)
 
     def part1(self):
-        return 0
+        result = 0
+        found = re.findall("mul\(\d+,\d+\)",self.input)
+        for f in found:
+            nums = re.findall("\d+",f)
+            result += int(nums[0]) * int(nums[1])
+        return result
 
     def part2(self):
         return 0
