1315de902b73b73833250cc3e7febb979f70515e
shayd3 <ryan.ross215@gmail.com>
2024-12-07 07:39:06 -0500

    day3 init and add aoc related files to gitignore

---
+++ day_3/day_3.py
@@ -0,0 +1,10 @@
+from day import Day
+class Day_3(Day):
+    def __init__(self):
+        self.input = self.puzzle_input(is_example_input=False)
+
+    def part1(self):
+        return 0
+
+    def part2(self):
+        return 0
