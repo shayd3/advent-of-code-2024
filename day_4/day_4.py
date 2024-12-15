0c73434e63ca9c1930ce11d9e9d532ac6104b0c5
shayd3 <ryan.ross215@gmail.com>
2024-12-15 13:45:19 -0500

    Day 4 Part 2 - Done!

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -9,14 +9,19 @@ class Day_4(Day):
     def parse_input(self):
         return Parse.to_2d_list(self.input)
 
-    def part1(self):
-        wordsearch = self.parse_input()
-        occurances = 0
+    def convert_to_coords(self, wordsearch):
         word_dict = defaultdict(str)
 
         for row_index, row in enumerate(wordsearch):
             for col_index, letter in enumerate(row):
                 word_dict[row_index, col_index] = letter
+
+        return word_dict
+
+    def part1(self):
+        wordsearch = self.parse_input()
+        occurances = 0
+        word_dict = self.convert_to_coords(wordsearch)
 
         coords = list(word_dict.keys())
         coord_offsets = -1,0,1
@@ -33,4 +38,22 @@ class Day_4(Day):
         return occurances
 
     def part2(self):
-        return 0
+        wordsearch = self.parse_input()
+        occurances = 0
+        word_dict = self.convert_to_coords(wordsearch)
+
+        coords = list(word_dict.keys())
+        coord_offsets = -1,0,1
+        possible_words = ["MAS", "SAM"]
+
+        for i,j in coords:
+            word = ""
+            word2 = ""
+            for offset in coord_offsets:
+                word += word_dict[i + offset, j + offset]
+                word2 += word_dict[i + offset, j - offset]
+            if word in possible_words and word2 in possible_words:
+                occurances += 1
+
+
+        return occurances
