2a6c5d4a9894ef92ec155ad64e73862a95865715
shayd3 <ryan.ross215@gmail.com>
2024-12-17 22:16:05 -0500

    day 6 init

---
+++ day_6/day_6.py
@@ -0,0 +1,14 @@
+# Day 6
+from day import Day
+from utilities.parse import Parse
+
+class Day_6(Day):
+    def __init__(self):
+        self.input = self.puzzle_input(is_example_input=True)
+
+    def part_1(self):
+        return
+
+    def part_2(self):
+        return
+
