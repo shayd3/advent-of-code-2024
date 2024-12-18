0ea995ab72848f0da6305fe0f804844931a9c3a0
shayd3 <ryan.ross215@gmail.com>
2024-12-17 22:07:06 -0500

    Day 5 Part 2 - Done

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -15,25 +15,41 @@ class Day_5(Day):
         page_nums = [list(map(int,nums.split(","))) for nums in page_nums.split('\n')]
         return page_rules_dict, page_nums
 
+    def is_ordered(self, page_nums, page_rules):
+        for i in range(1, len(page_nums)):
+            if page_nums[i] not in page_rules[page_nums[i-1]]:
+                return False
+        return True
+
     def part1(self):
         page_rules, page_nums_list = self.parse_input()
         valid_page_num_lists = []
 
         for page_nums in page_nums_list:
-            valid = True
-            for i in range(1, len(page_nums)):
-                if page_nums[i] not in page_rules[page_nums[i-1]]:
-                    valid = False
-                    break
-            if valid:
+            if self.is_ordered(page_nums, page_rules):
                 valid_page_num_lists.append(page_nums)
 
-        middle_page_count = 0
-        # get middle page number and add to middle_page_count
+        count = 0
         for valid_page_num_list in valid_page_num_lists:
-            middle_page_count += valid_page_num_list[len(valid_page_num_list)//2]
+            count += valid_page_num_list[len(valid_page_num_list)//2]
 
-        return middle_page_count
+        return count
 
     def part2(self):
-        return 0
+        page_rules, page_nums_list = self.parse_input()
+        invalid_page_num_lists = []
+        count = 0
+        # find invalid_page_num_lists
+        for page_nums in page_nums_list:
+            if not self.is_ordered(page_nums, page_rules):
+                invalid_page_num_lists.append(page_nums)
+
+        # Fix the invalid_page_num_lists
+        for invalid_page_num_list in invalid_page_num_lists:
+            while not self.is_ordered(invalid_page_num_list, page_rules):
+                for i in range(1, len(invalid_page_num_list)):
+                    if invalid_page_num_list[i] not in page_rules[invalid_page_num_list[i-1]]:
+                        invalid_page_num_list[i], invalid_page_num_list[i-1] = invalid_page_num_list[i-1], invalid_page_num_list[i]
+                        break
+            count += invalid_page_num_list[len(invalid_page_num_list)//2]
+        return count
